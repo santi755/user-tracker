a966c749a8c4a66ece9e643d9e5af71497a2125d
Santiago <santi755@gmail.com>
2023-11-18 17:02:03 +0100

    Update dependencies and remove unused code

--- dist/src/index.js
+++ dist/src/index.js
@@ -3,13 +3,5 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const path_1 = __importDefault(require("path"));
 const ErrorLogger_1 = __importDefault(require("./logger/ErrorLogger"));
-const logFilePath = path_1.default.join(__dirname, "file", "track.log");
-const logger = new ErrorLogger_1.default(logFilePath);
-logger.Warn("asdasdasdasd", "123.123.123.213");
-logger.Warn("asdasdasdasd", "123.123.123.213");
-logger.Warn("asdasdasdasd", "123.123.123.213");
-logger.Warn("asdasdasdasd", "123.123.123.213");
-logger.Warn("asdasdasdasd", "123.123.123.213");
-exports.default = logger;
+exports.default = ErrorLogger_1.default;
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,3 @@
-import path from "path";
 import ErrorLogger from "./logger/ErrorLogger";
 
-const logFilePath = path.join(__dirname, "file", "track.log");
-const logger = new ErrorLogger(logFilePath);
-
-logger.Warn("asdasdasdasd", "123.123.123.213");
-logger.Warn("asdasdasdasd", "123.123.123.213");
-logger.Warn("asdasdasdasd", "123.123.123.213");
-logger.Warn("asdasdasdasd", "123.123.123.213");
-logger.Warn("asdasdasdasd", "123.123.123.213");
-
-export default logger;
+export default ErrorLogger;
